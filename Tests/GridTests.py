7ef936b4954b70c3b6ac40516ef8be7ccd5682ce
Mario J. Schwaiger <mario.schwaiger@v2c2.at>
2024-10-29 20:58:18 +0100

    Vertikal geht

--- Tests/GridTests.py
+++ Tests/GridTests.py
@@ -21,34 +21,85 @@ class SimpleGridTest(unittest.TestCase):
     def setUp(self):
         rows, cols = (6, 7)
         self.emptygrid = [["0"]*cols]*rows
-        self.horizontalwin=[["0","0","0","0","0","0","0"],
+
+        self.horizontalnowin=[
+            ["0","0","0","0","0","x","x"],
+            ["x","x","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"]]
+        
+        self.horizontalwinx=[
+            ["0","0","0","0","0","0","0"],
             ["0","0","0","0","0","0","0"],
             ["0","0","0","0","0","0","0"],
             ["0","0","0","0","0","0","0"],
             ["0","0","0","0","0","0","0"],
             ["x","x","x","x","0","0","0"]]
-        self.verticalwin=[["0","0","0","0","0","0","0"],
+        self.horizontalwino=[
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["o","o","o","o","0","0","0"]]
+        self.verticalwinx=[
+            ["0","0","0","0","0","0","0"],
             ["0","0","0","0","0","0","0"],
             ["x","0","0","0","0","0","0"],
             ["x","0","0","0","0","0","0"],
             ["x","0","0","0","0","0","0"],
             ["x","0","0","0","0","0","0"]]
-    
-    def test_CheckHorizontalBottomWin(self):
-        cfw=CFW.CheckForWin(self.horizontalwin)
+        self.verticalwino=[
+            ["0","0","0","0","0","0","0"],
+            ["0","0","0","0","0","0","0"],
+            ["o","0","0","0","0","0","0"],
+            ["o","0","0","0","0","0","0"],
+            ["o","0","0","0","0","0","0"],
+            ["o","0","0","0","0","0","0"]]        
+
+    def test_CheckVerticalLeftWinX(self):
+        cfw=CFW.CheckForWin(self.verticalwinx)
+        self.assertEqual("x",cfw.checkVertically())
+
+    def test_CheckVerticalLeftWinO(self):
+        cfw=CFW.CheckForWin(self.verticalwino)
+        self.assertEqual("o",cfw.checkVertically())        
+
+    def test_CheckVerticalDraw(self):
+        cfw=CFW.CheckForWin(self.emptygrid)
+        self.assertEqual(False,cfw.checkVertically())
+
+    def test_CheckVerticalHorizontalWin(self):
+        cfw=CFW.CheckForWin(self.horizontalwinx)
+        self.assertEqual(False,cfw.checkVertically())          
+
+# Horizontale Testcases
+    def test_CheckHorizontalNoWin(self):
+        cfw=CFW.CheckForWin(self.horizontalnowin)
+        self.assertEqual(False,cfw.checkHorizontally())
+        
+    def test_CheckHorizontalBottomWinX(self):
+        cfw=CFW.CheckForWin(self.horizontalwinx)
         self.assertEqual("x",cfw.checkHorizontally())
+
+    def test_CheckHorizontalBottomWinO(self):
+        cfw=CFW.CheckForWin(self.horizontalwino)
+        self.assertEqual("o",cfw.checkHorizontally())        
 
     def test_CheckHorizontalBottomDraw(self):
         cfw=CFW.CheckForWin(self.emptygrid)
         self.assertEqual(False,cfw.checkHorizontally())
 
-    def test_CheckHorizontalBottomDrawVertically(self):
-        cfw=CFW.CheckForWin(self.verticalwin)
-        self.assertEqual(False,cfw.checkHorizontally())        
+    def test_CheckHorizontalBottomDrawVerticallyX(self):
+        cfw=CFW.CheckForWin(self.verticalwinx)
+        self.assertEqual(False,cfw.checkHorizontally())
         
+    def test_CheckHorizontalBottomDrawVerticallyO(self):
+        cfw=CFW.CheckForWin(self.verticalwino)
+        self.assertEqual(False,cfw.checkHorizontally())           
 
-    def test_assertFalse(self):
-        self.assertFalse(False)
 
     def no_test_cmd_output(self):
         CMDOutput.doCMDOutput(self.grid)
