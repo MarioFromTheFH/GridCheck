fe0405a0a596b4bc1c341b0e49ee61362cc85df6
Andreas-Mihail <andreas.cojoc.ac@gmail.com>
2025-02-02 12:58:26 +0100

    Kommentare im NumPy Standard

--- test/cmd_output.py
+++ test/cmd_output.py
@@ -13,16 +13,44 @@ __email__ = "s54953@edu.campus02.at"
 __status__ = "Development"
 
 
-class CMDOutput():
+class CMDOutput:
+    """
+    Eine Klasse zur Darstellung eines Gitters als Zeichenkettenausgabe für die Konsole.
+    """
+
     @staticmethod
     def doCMDOutput(grid):
-        outputstring=""
-        for row in reversed(grid):
-            outputstring+="|"
-            for coin in row:
-                outputstring+=coin
-                outputstring+="|"
-                
-            outputstring+="\n"
+        """
+        Erstellt eine textbasierte Darstellung des Gitters.
 
-        return outputstring
+        Diese Methode nimmt ein zweidimensionales Array `grid` entgegen und formatiert es
+        als Zeichenkette mit Spaltentrennzeichen (`|`), wobei die Reihen von unten nach oben
+        dargestellt werden.
+
+        Parameters
+        ----------
+        grid : list of list of str
+            Eine 2D-Liste, die das Raster repräsentiert. Jedes Element ist ein Zeichen,
+            das z. B. eine Spielfigur oder ein leeres Feld darstellen kann.
+
+        Returns
+        -------
+        str
+            Die formatierte Zeichenkettenrepräsentation des Gitters, bereit für die
+            Konsolenausgabe.
+        """
+        outputstring = ""  # Initialisierung der Ausgabekette
+
+        # Iteration durch das Gitter von unten nach oben
+        for row in reversed(grid):
+            outputstring += "|"  # Beginn der Zeile mit einem Spaltentrennzeichen
+
+            # Iteration durch die Elemente der aktuellen Zeile
+            for coin in row:
+                outputstring += coin  # Hinzufügen des Symbols zur Zeichenkette
+                outputstring += "|"  # Spaltentrenner nach jedem Symbol
+
+            outputstring += "\n"  # Zeilenumbruch nach jeder Reihe
+
+        return outputstring  # Rückgabe der formatierten Zeichenkette
+
